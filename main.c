87d5b44dfa109e12c54903cfb4c32060187d83a7
sreevanithimmigari <ee24mt022@iitdh.ac.in>
2024-09-23 00:26:23 +0530

    added int and function declarations

--- main.c
+++ main.c
@@ -2,3 +2,10 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
+volatile int i = 0;  // Used to measure the switch press time
+int duty;            // PWM duty cycle variable
+
+// Function Declarations
+void GPIO_PORTF_setup(void);   // Sets up GPIO Port F pins and interrupts
+void PWM_setup(void);          // Sets up the PWM module
+void Portf_interrupt_handler(void);  // Interrupt handler for switch presses
